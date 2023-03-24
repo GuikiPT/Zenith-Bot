8281c2f58e96b81210561ff4077097837f15fd43
GuikiPT <foxzillapt17@gmail.com>
2023-03-24 11:10:51 +0000

    Added better client configuration with more intents and partials

--- main.js
+++ main.js
@@ -27,7 +27,31 @@ runFiglet();
 
 const client = new Discord.Client({
     intents: [
+        Discord.GatewayIntentBits.AutoModerationConfiguration,
+        Discord.GatewayIntentBits.AutoModerationExecution,
+        Discord.GatewayIntentBits.DirectMessageReactions,
+        Discord.GatewayIntentBits.DirectMessages,
+        Discord.GatewayIntentBits.GuildEmojisAndStickers,
+        Discord.GatewayIntentBits.GuildIntegrations,
+        Discord.GatewayIntentBits.GuildInvites,
+        Discord.GatewayIntentBits.GuildMembers,
+        Discord.GatewayIntentBits.GuildMessageReactions,
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.GuildModeration,
+        Discord.GatewayIntentBits.GuildPresences,
+        Discord.GatewayIntentBits.GuildScheduledEvents,
+        Discord.GatewayIntentBits.GuildVoiceStates,
+        Discord.GatewayIntentBits.GuildWebhooks,
         Discord.GatewayIntentBits.Guilds,
+        Discord.GatewayIntentBits.MessageContent,
+    ],
+    partials: [
+        Discord.Partials.Channel,
+        Discord.Partials.GuildMember,
+        Discord.Partials.GuildScheduledEvent,
+        Discord.Partials.Message,
+        Discord.Partials.Reaction,
+        Discord.Partials.ThreadMember,
     ]
 });
 
