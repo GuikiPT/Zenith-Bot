e672ce530414f3cba4140587c7ac28b73b53638b
GuikiPT <foxzillapt17@gmail.com>
2023-03-28 09:28:54 +0100

    Created simple ping command

---
+++ commands/slash/information/ping.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+
+module.exports = {
+	data: new Discord.SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Reply with bot latency!'),
+	async execute(interaction) {
+        const LoadingMessage = await interaction.reply({ content: 'Pinging...', ephemeral: false, fetchReply: true });
+        const BotLatency = LoadingMessage.createdTimestamp - interaction.createdTimestamp;
+        
+        const PingEmbed = new Discord.EmbedBuilder()
+            .setTitle('🏓 | Pong!')
+            .setColor('Random')
+            .setThumbnail(interaction.client.user.displayAvatarURL({ size: 2048, format: 'png', dynamic: true }))
+            .addFields(
+                { name: 'Bot Latency', value: '```ini\n [' + BotLatency + 'ms ]```', inline: true },
+                { name: 'API Latency', value: '```ini\n [' + client.ws.ping + 'ms ]```', inline: true }
+            )
+            .setTimestamp()
+        return await interaction.editReply({ content: '', embeds: [PingEmbed] });
+    },
+};
