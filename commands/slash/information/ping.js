f4aebbdbcb387e585fd8e601f53455125804ba09
GuikiPT <foxzillapt17@gmail.com>
2023-03-28 09:43:45 +0100

    Added try catch

--- commands/slash/information/ping.js
+++ commands/slash/information/ping.js
@@ -1,22 +1,28 @@
 const Discord = require('discord.js');
+const colors = require('colors/safe');
 
 module.exports = {
 	data: new Discord.SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Reply with bot latency!'),
 	async execute(interaction) {
-        const LoadingMessage = await interaction.reply({ content: 'Pinging...', ephemeral: false, fetchReply: true });
-        const BotLatency = LoadingMessage.createdTimestamp - interaction.createdTimestamp;
-        
-        const PingEmbed = new Discord.EmbedBuilder()
-            .setTitle('🏓 | Pong!')
-            .setColor('Random')
-            .setThumbnail(interaction.client.user.displayAvatarURL({ size: 2048, format: 'png', dynamic: true }))
-            .addFields(
-                { name: 'Bot Latency', value: '```ini\n [' + BotLatency + 'ms ]```', inline: true },
-                { name: 'API Latency', value: '```ini\n [' + client.ws.ping + 'ms ]```', inline: true }
-            )
-            .setTimestamp()
-        return interaction.editReply({ content: '', embeds: [PingEmbed] });
+        try {
+            const LoadingMessage = await interaction.reply({ content: 'Pinging...', ephemeral: false, fetchReply: true });
+            const BotLatency = LoadingMessage.createdTimestamp - interaction.createdTimestamp;
+            
+            const PingEmbed = new Discord.EmbedBuilder()
+                .setTitle('🏓 | Pong!')
+                .setColor('Random')
+                .setThumbnail(interaction.client.user.displayAvatarURL({ size: 2048, format: 'png', dynamic: true }))
+                .addFields(
+                    { name: 'Bot Latency', value: '```ini\n [' + BotLatency + 'ms ]```', inline: true },
+                    { name: 'API Latency', value: '```ini\n [' + client.ws.ping + 'ms ]```', inline: true }
+                )
+                .setTimestamp()
+            return interaction.editReply({ content: '', embeds: [PingEmbed] });
+        }
+        catch (error) {
+            console.log(colors.red(error.stack));
+        }
     },
 };
